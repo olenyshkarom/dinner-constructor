863f8db7bfebe3fbedf30a4fd6307165673c0b3c
olenyshka <eyasya@rambler.ru>
2024-05-15 08:54:04 +0300

    правки1

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -6,26 +6,23 @@ import java.util.Random;
 
 public class DinnerConstructor {
     HashMap<String, ArrayList<String>> dishes;    // = new HashMap<>();
+    private Random random = new Random();       //Спасибо!!!
 
     public DinnerConstructor() {
         dishes = new HashMap<>();
     }
 
     public void addDish(String type, String name) {
+        ArrayList<String> arrDishes;
         //1. Такой тип в мапе уже есть -> Получить из мапы список с блюдами и добавить в него новое
         //2. Такого типа в мапе еще пока нет -> Создать список, положить в него новое блюдо и положить в мапу
         if (dishes.containsKey(type)) {
-            ArrayList<String> arrDishes = dishes.get(type);
-            if (arrDishes.contains(name)) {
-                System.out.println(name + " уже есть");
-                return;
-            }
-            arrDishes.add(name);
+            arrDishes = dishes.get(type);
         } else {
-            ArrayList<String> arrDishes = new ArrayList<>();
-            arrDishes.add(name);
+            arrDishes = new ArrayList<>();
             dishes.put(type, arrDishes);
         }
+        arrDishes.add(name);
         System.out.println(type + " " + name + " добавили");
     }
 
@@ -33,20 +30,19 @@ public class DinnerConstructor {
     public ArrayList<ArrayList<String>> generateCombinations(int cnt, ArrayList<String> types) {
 
         ArrayList<ArrayList<String>> combos = new ArrayList<>();
-        Random rand = new Random();
+
 
         for (int i = 0; i < cnt; i++) {
             ArrayList<String> combo = new ArrayList<>();
             for (String type : types) {
                 ArrayList<String> dishType = dishes.get(type);
 
-                int randType = rand.nextInt(dishType.size());
+                int randType = random.nextInt(dishType.size());
 
                 combo.add(dishType.get(randType));
             }
             combos.add(combo);
         }
-
         return combos;
         // В цикле for пройти cnt раз и сгенерировать на каждой итерации по комбинации
         // Пройтись по всем типам, из которых нужно сделать комбинацию
@@ -61,6 +57,5 @@ public class DinnerConstructor {
     public boolean checkType(String type) {
         return dishes.containsKey(type);
     }
-
 
 }
